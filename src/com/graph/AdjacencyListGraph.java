a0d3453c593553f2a3367273ae7e74e46db989a0
Jeff Test <jwong@livevox.com>
2012-01-12 11:37:49 -0800

    all tests pass for adjacency list

--- src/com/graph/AdjacencyListGraph.java
+++ src/com/graph/AdjacencyListGraph.java
@@ -5,15 +5,25 @@ import java.util.List;
 
 public class AdjacencyListGraph implements Graph {
 
+	private class ConnectedNode{
+		public int node;
+		public int distance;
+		ConnectedNode(int node, int distance){
+			this.node = node;
+			this.distance = distance;
+		}
+	}
+	
 	private int size;
 	
-	private List<Integer>[] adjacencyList;
+	private List<ConnectedNode>[] adjacencyList;
 	
+	@SuppressWarnings("unchecked")
 	public AdjacencyListGraph(int size) {
 		this.size = size;
-		this.adjacencyList = (ArrayList<Integer>[])new ArrayList[size];
+		this.adjacencyList = (ArrayList<ConnectedNode>[])new ArrayList[size];
 		for(int i = 0; i < size; i++){
-			this.adjacencyList[i] = new ArrayList<Integer>();
+			this.adjacencyList[i] = new ArrayList<ConnectedNode>();
 		}
 	}
 
@@ -21,26 +31,40 @@ public class AdjacencyListGraph implements Graph {
 	public void connect(int vertex1, int vertex2) {
 		checkIndexOutOfBounds(vertex1);
 		checkIndexOutOfBounds(vertex2);
-		
+		connect(vertex1, vertex2, 1);
 	}
 
 	@Override
 	public void connect(int vertex1, int vertex2, int distance) {
 		checkIndexOutOfBounds(vertex1);
 		checkIndexOutOfBounds(vertex2);
-
+		this.adjacencyList[vertex1].add(new ConnectedNode(vertex2, distance));
+		this.adjacencyList[vertex2].add(new ConnectedNode(vertex1, distance));
 	}
 
 	@Override
 	public List<Integer> getConnectedList(int vertex) {
 		checkIndexOutOfBounds(vertex);
-		return this.adjacencyList[vertex];
+		List<Integer> list = new ArrayList<Integer>();
+		
+		List<ConnectedNode> connectedList = this.adjacencyList[vertex];
+		for(int i = 0; i < connectedList.size();i++){
+			list.add(connectedList.get(i).node);
+		}
+		
+		return list;
 	}
 
 	@Override
 	public Integer getDistance(int vertex1, int vertex2) {
 		checkIndexOutOfBounds(vertex1);
 		checkIndexOutOfBounds(vertex2);
+		List<ConnectedNode> connectedList = this.adjacencyList[vertex1];
+		for(int i = 0; i < connectedList.size();i++){
+			if(connectedList.get(i).node == vertex2){
+				return connectedList.get(i).distance;
+			}
+		}
 		return null;
 	}
 
@@ -48,6 +72,12 @@ public class AdjacencyListGraph implements Graph {
 	public boolean isConnected(int vertex1, int vertex2) {
 		checkIndexOutOfBounds(vertex1);
 		checkIndexOutOfBounds(vertex2);
+		List<ConnectedNode> connectedList = this.adjacencyList[vertex1];
+		for(int i = 0; i < connectedList.size();i++){
+			if(connectedList.get(i).node == vertex2){
+				return true;
+			}
+		}
 		return false;
 	}
 

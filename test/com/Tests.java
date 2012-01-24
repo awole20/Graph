258156029fbfc72f3520df0c63761c29469b3969
Jeff Wong <awole20@gmail.com>
2012-01-23 17:03:35 -0800

    adding bfs test to suite

--- test/com/Tests.java
+++ test/com/Tests.java
@@ -3,11 +3,12 @@ package com;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import com.bfs.TestBfs;
 import com.dijkstra.DijkstraTest;
 import com.graph.GraphTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({GraphTest.class, DijkstraTest.class})
+@Suite.SuiteClasses({GraphTest.class, DijkstraTest.class, TestBfs.class})
 public class Tests {
 
 }

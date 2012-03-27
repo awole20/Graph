8a93e88b4bb3870be041cf702d1d33db4cfa3e49
Jeff Wong <awole20@gmail.com>
2012-03-26 17:42:40 -0700

    adding stub for object graph TODO: make generic graph out of existing classes
    Change-Id: I383bbf4bf2d3ff7e205bc7e838041ccb00a5b754

---
+++ src/com/graph/DirectedObjectGraph.java
@@ -0,0 +1,12 @@
+package com.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DirectedObjectGraph<k> implements DirectedGraph{
+
+	private DirectedGraph graph;
+	private Map<k,Integer> objectMap = new HashMap<k,Integer>();
+	
+	
+}
---
+++ test/com/graph/NamedDirectedGraphTest.java
@@ -0,0 +1,14 @@
+package com.graph;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NamedDirectedGraphTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

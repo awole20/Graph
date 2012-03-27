15cef24e55a3f8319c54c5c4ebe4cfcfe05309b7
Jeff Wong <awole20@gmail.com>
2012-03-27 09:55:15 -0700

    completing directed object graph implementation
    Change-Id: I08364310d0be0c57dea8c47dbbf34bca7efa3729

--- src/com/graph/DirectedObjectGraph.java
+++ src/com/graph/DirectedObjectGraph.java
@@ -3,20 +3,22 @@ package com.graph;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DirectedObjectGraph<k> implements DirectedGraph{
+public class DirectedObjectGraph<k> implements DirectedGraph<k>{
 
 	private IntDirectedGraph graph;
 	private List<k> objectList = new ArrayList<k>();
 	
-	public DirectedObjectGraph(List listObjects){
+	public DirectedObjectGraph(List<k> listObjects){
 		this.objectList = listObjects;
 		this.graph = new AdjacencyListDirectedGraph(this.objectList.size());
 	}
 	
+	@Override
 	public void connect(k vertex1, k vertex2) {
 		this.connect(vertex1, vertex2, 1);
 	}
 	
+	@Override
 	public void connect(k vertex1, k vertex2, int distance) {
 		int vertex1pos = this.objectList.indexOf(vertex1);
 		int vertex2pos = this.objectList.indexOf(vertex1);
@@ -25,9 +27,9 @@ public class DirectedObjectGraph<k> implements DirectedGraph{
 		} catch(IndexOutOfBoundsException e){
 			throw new IndexOutOfBoundsException("Node not found");
 		}
-		
 	}
 	
+	@Override
 	public boolean isConnected(k vertex1, k vertex2){
 		int vertex1pos = this.objectList.indexOf(vertex1);
 		int vertex2pos = this.objectList.indexOf(vertex1);
@@ -37,31 +39,29 @@ public class DirectedObjectGraph<k> implements DirectedGraph{
 			throw new IndexOutOfBoundsException("Node not found");
 		}
 	}
-	
+
 	@Override
-	public List<Integer> getConnectedList(int vertex) {
-		return this.graph.getConnectedList(vertex);
+	public List<k> getConnectedList(k vertex) {
+		List<k> connectedValues = new ArrayList<k>();
+		int vertexPos = this.objectList.indexOf(vertex);
+		List<Integer> positionList = this.graph.getConnectedList(vertexPos);
+		for(int i = 0; i < positionList.size(); i++){
+			connectedValues.add(this.objectList.get(positionList.get(i)));
+		}
+		return connectedValues;
 	}
+
 	@Override
-	public Integer getDistance(int vertex1, int vertex2) {
-		return this.graph.getDistance(vertex1, vertex2);
+	public Integer getDistance(k vertex1, k vertex2) {
+		int vertex1pos = this.objectList.indexOf(vertex1);
+		int vertex2pos = this.objectList.indexOf(vertex1);
+		
+		return this.graph.getDistance(vertex1pos, vertex2pos);
 	}
-	@Override
-	public boolean isConnected(int vertex1, int vertex2) {
-		return this.graph.isConnected(vertex1, vertex2);
-	}
+
 	@Override
 	public int size() {
 		return this.graph.size();
-	}
-	@Override
-	public void connect(int vertex1, int vertex2) {
-		this.graph.connect(vertex1, vertex2);
-	}
-	@Override
-	public void connect(int vertex1, int vertex2, int distance) {
-		this.graph.connect(vertex1, vertex2, distance);
-		
 	}
 	
 	
